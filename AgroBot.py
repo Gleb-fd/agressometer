1076155a1fd6934ab53a25d4db3d8b52099b8635
Gleb Bakulev <73277953+Gleb-fd@users.noreply.github.com>
2023-08-03 21:55:21 +0500

    Update AgroBot.py

--- AgroBot.py
+++ AgroBot.py
@@ -2,6 +2,8 @@ import discord
 import torch
 from discord.ext import commands
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import random
+import json
 
 from settings import settings_bot
 
@@ -10,11 +12,14 @@ tokenizer = AutoTokenizer.from_pretrained(PATH)
 model = AutoModelForSequenceClassification.from_pretrained(PATH)
 model.to(torch.device("cpu"))
 
-warning_threshold = 60
+warning_threshold = 65
 user_warnings = {}
+whitelisted_users = set()
+chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='*', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 
 @bot.event
@@ -37,6 +42,9 @@ async def on_message(message):
     if message.content.startswith('*'):
         return
 
+    if message.author.id in whitelisted_users:
+        return
+
     toxicity_probs = get_toxicity_probs(message.content)
     toxicity_perce = toxicity_probs[1] * 100
 
@@ -55,6 +63,35 @@ async def on_message(message):
         await user.send(warning_msg)
 
     await bot.process_commands(message)
+
+
+@bot.command()
+async def out(ctx, *, password):
+    print(generated_password)
+    if password == generated_password:
+        whitelisted_users.add(ctx.author.id)
+        save_whitelisted_users()
+        await ctx.send(f'{ctx.author.mention} Вы добавлены в список игнорирования ботом.')
+    else:
+        await ctx.send(f'{ctx.author.mention} Неправильный код. Пожалуйста, попробуйте еще раз.')
+
+generated_password = ''.join(random.choice(chars) for _ in range(5))
+
+
+def save_whitelisted_users():
+    with open("whitelisted_users.json", "w") as f:
+        json.dump(list(whitelisted_users), f)
+
+
+def load_whitelisted_users():
+    try:
+        with open("whitelisted_users.json", "r") as f:
+            return set(json.load(f))
+    except FileNotFoundError:
+        return set()
+
+
+whitelisted_users = load_whitelisted_users()
 
 
 bot_token = settings_bot['token']
